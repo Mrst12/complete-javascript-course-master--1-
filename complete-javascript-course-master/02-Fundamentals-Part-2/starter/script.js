a28b4d5082331ac2a338b0ea0e042a48207765b6
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2022-11-17 13:46:59 +0000

    add lecture notes on functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function logger() {
+    console.log('My name is Lisa');
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+fruitProcessor(5, 0);
+// to get the string above we have to store result in a variable
+const appleJuice = fruitProcessor(5, 0);//function captured into a variable
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));//result of running the function
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
