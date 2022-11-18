17a6fa9d3bfcecbe8e3be147e10eff1820b74fad
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2022-11-18 13:45:55 +0000

    add lecture notes for reviewing functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -67,7 +67,7 @@ console.log(yearsUntilRetirement(1980, 'Bob'));
 */
 
 //functions calling other functions
-
+/*
 function cutFruitPieces(fruit) {
     return fruit * 4;
 }
@@ -80,3 +80,23 @@ function fruitProcessor(apples, oranges) {
 }
 
 console.log(fruitProcessor(2, 3));
+*/
+/*
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        return retirement;
+    } else {
+        return -1;
+    }
+}
+
+console.log(yearsUntilRetirement(1991, 'Lisa'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+*/
