0dc58270127ae7618f1bdb6017c7f25e73e6b118
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2022-11-24 19:44:58 +0000

    add lecture notes for backward loops and loops inside loops

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -488,3 +488,26 @@ for (let i = 0; i < jonas.length; i++) {
     console.log(jonas[i], typeof jonas[i]);
 }
 */
+
+// looping backwards and loops in loops
+/*
+const jonas = [
+    'jonas',
+    'schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+];
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+    console.log(jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`-----Starting exercise ${exercise}`)
+
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`)
+    }
+};
+*/
